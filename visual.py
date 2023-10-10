3dcfdf223f6a5aa4aa0568d980473ded1441ae56
Rahul <rahukand@gmail.com>
2023-10-10 11:47:36 -0700

    Extracted visualization code to a seperate .py file to be reused easily

--- visual.py
+++ visual.py
@@ -1,113 +1,90 @@
-'''
-Run this file to see a visualization of anglez and enmo of a child sleeping
-(Deprecated) Use code from visualizer.ipynb instead
-'''
-# %% [markdown]
-# # Visualizing the data
-
-# %%
-import pandas as pd
-import dataloader
-import os
-
-# %%
-events = pd.read_csv(os.path.join('data', 'train_events.csv'))
-# events.head()
-
-# %%
-nacheck = events.isna()
-nacheck['series_id'] = events['series_id']
-nacount = nacheck.groupby('series_id').sum()
-good_sids = nacount[nacount['step']==0].index
-# good_sids
-
-# %%
-sid = good_sids[0]
-# sid
-
-# %%
-# dataloader.init()
-
-# %%
-acc_data = dataloader.acc_data_for_child(sid)
-
-# %%
 import matplotlib.pyplot as plt
-import seaborn as sns
-
-# %%
-# fig, ax = plt.subplots()
-# sns.scatterplot(data=acc_data, x='anglez', y='enmo', marker='.')
-
-# %%
-# lets figure out how to categorize this as sleeping or awake
-# given a timestamp is it awake or asleep
-# events[events['series_id']]
-event = events[events['series_id']==sid]['event']
-step = events[events['series_id']==sid]['step']
-# Lets ensure that the events are all `onset then wakeup`
-assert set([i==('onset', 'wakeup') for i in zip(event[:-1:2], event[1::2])]) == set([True])
-sleeping = sum([(acc_data['step'] >= onset) & (acc_data['step'] < wakeup) for onset, wakeup in zip(step[:-1:2], step[1::2])])
-# sleeping
-
-# %%
-acc_data['activity'] = ['Sleeping' if i else 'Waking' for i in sleeping]
-# acc_data.sample(10)
-
-# %%
-# fig, ax = plt.subplots()
-# sns.scatterplot(ax=ax, data=acc_data, x='anglez', y='enmo', hue='activity', marker='.')
-
-# %%
-# fig, axs = plt.subplots(nrows=2)
-# sns.scatterplot(ax=axs[0], data=acc_data.loc[acc_data['activity']=='Waking'], x='anglez', y='enmo')
-# axs[0].set_title('Waking')
-# sns.scatterplot(ax=axs[1], data=acc_data.loc[acc_data['activity']=='Sleeping'], x='anglez', y='enmo')
-# axs[1].set_title('Sleeping')
-# fig.tight_layout()
-
-# %%
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+# from matplotlib.cm import ScalarMappable
 import matplotlib.animation as anim
-import matplotlib.axes as axes
-import tqdm
+from matplotlib.collections import PathCollection
+import pandas as pd
+import numpy as np
 
-fig, ax = plt.subplots()
-ax: axes.Axes = ax
-ax.set_xlim(-100, 100)
-ax.set_ylim(-0.5, 3.5)
-ax.set_xlabel('anglez')
-ax.set_ylabel('enmo')
-point = ax.scatter(2, 1, marker='o')
-txt = ax.text(0, 0, 'Some Text')
-# title = ax.set_title("This is title")
+class Visualizaer1:
+    def init_frame(ax: Axes, data: pd.DataFrame):
+        '''
+        Create an empty figure with containers for everything that will go in it.
 
-tq = tqdm.tqdm(range(len(acc_data)))
-tqiter = iter(tq)
+        Returns
+        -------
+        A list of the artists
+        '''
+        points = ax.scatter(x=[], y=[], c=[], cmap='plasma')
+        ax.set_xlabel('anglez')
+        ax.set_ylabel('enmo')
+        xmin = data['anglez'].min()
+        xmax = data['anglez'].max()
+        buff = (xmax-xmin)*0.1
+        ax.set_xlim(xmin-buff, xmax+buff)
+        ymin = data['enmo'].min()
+        ymax = data['enmo'].max()
+        buff = (ymax-ymin)*0.1
+        ax.set_ylim(ymin-buff, ymax+buff)
+        activity_text = ax.text((xmax+xmin)/2, ymax-data['enmo'].std(), "SOME TEXT", ha='center', weight='bold', va='bottom')
+        return [points, activity_text]
 
+    def draw_frame(ts: pd.Timestamp, data: pd.DataFrame, window: pd.Timedelta, x_jit_scale: int, y_jit_scale: int, points: PathCollection, activity_text: plt.Text):
+        artists = []
+        if type(ts) is not pd.Timestamp:
+            ts = pd.Timestamp(ts)
+        idx = (data['timestamp'] >= ts-window) & (data['timestamp'] < ts+window)
+        data = data.loc[idx, ['step', 'anglez', 'enmo', 'activity']]
+        # Set X, Y points
+        x = data['anglez']
+        y = data['enmo']
+        dat_len = len(data)
+        mid = data['step'].iat[int(dat_len/2)]
+        c = [i - mid for i in data['step']]
+        x_jit = (np.random.random_sample(x.shape)-0.5)*x_jit_scale
+        y_jit = (np.random.random_sample(y.shape)-0.5)*y_jit_scale
+        jit = 0.3
+        points.set(offsets=list(zip(x+x_jit*jit,y+y_jit*jit)), array=c)
+        artists.append(points)
+        # Set the text box
+        txt = f"{ts}\n{data['activity'].iat[0]} => {data['activity'].iat[-1]}"
+        activity_text.set_text(txt)
+        artists.append(activity_text)
+        return artists
 
+class Visualizer2:
+    def init_frame(ax:Axes, window):
+        # fig.colorbar(ScalarMappable(np.linspace))
+        ax.axhline(0, color='k')
+        points = ax.scatter(x=[], y=[], c=[], cmap='plasma', vmin=0, vmax=0.2)
+        ax.set_xlabel('anglez')
+        ax.set_xlim(-95, 95)
+        ax.set_ylabel('Minute Offset from now')
+        window = window.seconds/60
+        ax.set_ylim(-1.1*(window/2), 1.3*(window/2))
+        # ax.set_ylim(-100, 100)#(window/2))
+        activity_text = ax.text(0, 1*window/2, "SOME\nTEXT", ha='center', va='bottom', weight='bold')
+        return [points, activity_text]
+    def draw_frame(ts: pd.Timestamp, full_data: pd.DataFrame, jitter:float, window, artists: list):
+        art2=[]
+        if type(ts) is not pd.Timestamp:
+            ts = pd.Timestamp(ts)
+        data = full_data
+        idx = (data['timestamp'] >= ts-window/2) & (data['timestamp'] < ts+window/2)
+        data = data.loc[idx, ['step', 'anglez', 'enmo', 'activity', 'timestamp']]
+        # Set X points
+        x = data['anglez']
+        x += (np.random.random_sample(x.shape)-0.5)*jitter*full_data['anglez'].std()
 
-def animate(step):
-    if step==0:
-        global tqiter, tq
-        tq = tqdm.tqdm(range(len(acc_data)))
-        tqiter = iter(tq)
-    x, y, activity, ts = acc_data.loc[acc_data['step']==step+1, ['anglez', 'enmo', 'activity', 'timestamp']].iloc[0]
-    # x,y = 0,0
-    point.set_offsets((x, y))
-    point.set_color('orange' if activity=='Waking' else 'blue')
-    txt.set_text(f'{activity}, {ts}')
-    # title.set_text(f'{activity}, {ts}')
-    # print((x,y))
-    # next(tqiter)
-    next(tqiter)
-    return [point, txt, title]
-
-ani = anim.FuncAnimation(fig, animate, frames=len(acc_data), interval=1, blit=True)
-
-# ani.save('scatter.gif')
-print("Showing Plot")
-plt.show()
-print("Finished Showing Figure")
-
+        # Set Y points
+        step = data.loc[data['timestamp']==ts, 'step'].iat[0]
+        y = [(i-step)/12 for i in data['step']]
+        artists[0].set(offsets=list(zip(x,y)), array=data['enmo'])
+        art2.append(artists[0])
+        txt = f"{ts}\n{data['activity'].iat[0]} => {data['activity'].iat[-1]}"
+        artists[1].set_text(txt)
+        art2.append(artists[1])
+        return art2
 
 
