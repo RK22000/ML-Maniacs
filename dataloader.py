0d55334c13132797857ee683a1844acb681fe1d8
Rahul <rahukand@gmail.com>
2023-11-13 02:31:43 -0800

    Smoothened out the model predictions into cleaner curves. Implemented checkpoints on the model so we can just load a previously trained model instead of retraining one everytime. Tested out the model by predicting on a different child from the trained child, and visualized the results in full_pred_convo_40_min.mp4

--- dataloader.py
+++ dataloader.py
@@ -103,9 +103,14 @@ def annotate_sid(acc_data, events, sid):
     sid: The series id from events being used to annotate
     '''
     event = events[events['series_id']==sid]['event']
+    a = event.copy().iloc[:-1]
+    b = event.copy().iloc[1:]
+    b.index -= 1
+    # Ensure none of the adjacent elements are the same event
+    assert sum(a==b) == 0
     step = events[events['series_id']==sid]['step']
     # Lets ensure that the events are all `onset then wakeup`
-    assert set([i==('onset', 'wakeup') for i in zip(event[:-1:2], event[1::2])]) == set([True])
+    # assert set([i==('onset', 'wakeup') for i in zip(event[:-1:2], event[1::2])]) == set([True])
     sleeping = sum([(acc_data['step'] >= onset) & (acc_data['step'] < wakeup) for onset, wakeup in zip(step[:-1:2], step[1::2])])
     acc_data['activity'] = ['Sleeping' if i else 'Waking' for i in sleeping]
 
